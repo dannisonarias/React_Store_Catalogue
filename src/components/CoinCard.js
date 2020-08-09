0d2b3d2957f4ee46899e3b477a4043a992d34720
DannisonArias <dannisonarias@gmail.com>
2020-08-09 09:40:53 -0400

    update CoinCard

--- src/components/CoinCard.js
+++ src/components/CoinCard.js
@@ -1,32 +1,53 @@
 import React from 'react';
 import { Card, Icon, Image } from 'semantic-ui-react';
 
-const Coin = props => {
-  const { coin } = props;
-  let rank;
-  if (!coin.rank || parseInt(coin.rank) > 5) {
-    rank = 5;
-  } else {
-    rank = parseInt(coin.rank);
+class Coin extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.handleClick = this.handleClick.bind(this);
   }
-  coin.logo_url = (!coin.logo_url == '') ? coin.logo_url : `/assets/images/no-image.jpg`;
 
-  return (
-    <Card>
-      <img src={coin && coin.logo_url} height={150} alt="Coin Logo" />
-      <Card.Content>
-        <Card.Header>{coin && coin.name}</Card.Header>
-        <Card.Meta>
-          {coin && coin.symbol}
-        </Card.Meta>
-      </Card.Content>
-      <Card.Content extra>
-        <Icon name="user" />
-        Popularity:
-        {'⭐'.repeat([5 - rank + 1]).slice(0, 5)}
-      </Card.Content>
-    </Card>
-  );
-};
+  handleClick(e) {
+    const uiCard = e.target.closest('.ui .card').id;
+  }
+
+  setStars(coin) {
+    let stars;
+    if (!coin.rank || parseInt(coin.rank) > 5) {
+      stars = 5;
+    } else {
+      stars = parseInt(coin.rank);
+    }
+    return stars;
+  }
+
+  setDefaultLogo(coin){
+    return (!coin.logo_url == '') ? coin.logo_url : '/assets/images/no-image.jpg';
+  }
+
+  render() {
+    const { coin } = this.props;
+    const rank = this.setStars(coin);
+    coin.logo_url = this.setDefaultLogo(coin);
+
+    return (
+      <Card onClick={this.handleClick} id={coin.symbol}>
+        <img src={coin && coin.logo_url} height={150} alt="Coin Logo" />
+        <Card.Content>
+          <Card.Header>{coin && coin.name}</Card.Header>
+          <Card.Meta>
+            {coin && coin.symbol}
+          </Card.Meta>
+        </Card.Content>
+        <Card.Content extra>
+          <Icon name="user" />
+          Popularity:
+          {'⭐'.repeat([5 - rank + 1]).slice(0, 5)}
+        </Card.Content>
+      </Card>
+    );
+  }
+}
 
 export default Coin;
