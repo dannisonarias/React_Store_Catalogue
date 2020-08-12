a953e262804f72e2ccddafa8aee0dd52098cf31b
DannisonArias <dannisonarias@gmail.com>
2020-08-12 10:36:25 -0400

    stylelint

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import 'react-bootstrap';
 import './style.css';
 
-const Footer = props => (
+const Footer = () => (
   <footer className="site-footer mt-5 p-5">
     <div className="container1">
       <div className="row">
@@ -10,35 +10,36 @@ const Footer = props => (
           <h6>About</h6>
           <p className="text-justify">
             Dannison Arias
-            {' '}
+            &nbsp;
             <i>SIMPLY AMAZING </i>
-            {' '}
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-            {' '}
+            &nbsp;
+            {/* eslint-disable-next-line max-len */}
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            &nbsp;
           </p>
         </div>
 
         <div className="col-sm-6 col-md-3 p-1">
           <h6>My Skills</h6>
           <ul className="footer-links">
-            <li><a href="">Ruby on Rails</a></li>
-            <li><a href="">Javascript</a></li>
-            <li><a href="">React</a></li>
-            <li><a href="">UI Design</a></li>
-            <li><a href="">Github</a></li>
-            <li><a href="">Aws</a></li>
-            <li><a href="">and More</a></li>
+            <li><a href="https://dannisonarias.github.io/">Ruby on Rails</a></li>
+            <li><a href="https://dannisonarias.github.io/">Javascript</a></li>
+            <li><a href="https://dannisonarias.github.io/">React</a></li>
+            <li><a href="https://dannisonarias.github.io/">UI Design</a></li>
+            <li><a href="https://dannisonarias.github.io/">Github</a></li>
+            <li><a href="https://dannisonarias.github.io/">Aws</a></li>
+            <li><a href="https://dannisonarias.github.io/">and More</a></li>
           </ul>
         </div>
 
         <div className="col-sm-6 col-md-3 p-1">
           <h6>Quick Links</h6>
           <ul className="footer-links">
-            <li><a href="">About Us</a></li>
-            <li><a href="">Contact Us</a></li>
-            <li><a href="">Contribute</a></li>
-            <li><a href="">Privacy Policy</a></li>
-            <li><a href="">Sitemap</a></li>
+            <li><a href="https://dannisonarias.github.io/">About Us</a></li>
+            <li><a href="https://dannisonarias.github.io/">Contact Us</a></li>
+            <li><a href="https://dannisonarias.github.io/">Contribute</a></li>
+            <li><a href="https://dannisonarias.github.io/">Privacy Policy</a></li>
+            <li><a href="https://dannisonarias.github.io/">Sitemap</a></li>
           </ul>
         </div>
       </div>
@@ -56,13 +57,13 @@ const Footer = props => (
         <div className="col-md-4 col-sm-6 col-xs-12">
           <ul className="social-icons">
             <li>
-              <a className="facebook" href="#">
+              <a className="facebook" href="https://dannisonarias.github.io/">
                 <i className="fa fa-facebook" />
               </a>
             </li>
-            <li><a className="twitter" href="#"><i className="fa fa-twitter" /></a></li>
-            <li><a className="dribbble" href="#"><i className="fa fa-dribbble" /></a></li>
-            <li><a className="linkedin" href="https://www.linkedin.com/in/dannisonarias/"><i className="fa fa-linkedin" /></a></li>
+            <li><a className="twitter" href="https://dannisonarias.github.io/" alt="link"><i className="fa fa-twitter" /></a></li>
+            <li><a className="dribbble" href="https://dannisonarias.github.io/" alt="link"><i className="fa fa-dribbble" /></a></li>
+            <li><a className="linkedin" href="https://www.linkedin.com/in/dannisonarias/" alt="link"><i className="fa fa-linkedin" /></a></li>
           </ul>
         </div>
       </div>
